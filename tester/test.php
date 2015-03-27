073c9c000d42d8f92f2b6f45506cc1ade5f9f8fc
BradCzumo <bradley.czumaczenko@mail.utoronto.ca>
2015-03-27 03:28:21 -0400

    Next Image

--- tester/test.php
+++ tester/test.php
@@ -1,137 +1,124 @@
 <?php
 /*
- * Plugin Name: tester
- * Plugin URI: http://codediva.com
- * Description: This is my awesome plugin
- * Author: Code Diva
+ * Plugin Name: Image Enlarger
+ * Plugin URI: https://phoenix.sheridanc.on.ca/~ccit2639/?page_id=146
+ * Description: This is a photo enlarger
+ * Author: bradusach
  * Version: 1.0
- * Author URI: http://codediva.com
+ * Author URI: http://phoenix.sheridanc.on.ca/~ccit2639
  */
 
 
-function cd_awesome_add_admin_menu(  ) { 
+function bds_awesome_add_admin_menu(  ) { 
 
-	add_menu_page( 'My Awesome Plugin', 'My Awesome Plugin', 'manage_options', 'my_awesome_plugin', 'my_awesome_plugin_options_page', 'dashicons-hammer', 66 );
+	add_menu_page( 'Image Enlarger', 'Image Enlarger', 'manage_options', 'my_awesome_plugin', 'my_awesome_plugin_options_page', 'dashicons-hammer', 66 );
 
 }
 
 
-function cd_awesome_settings_init(  ) { 
+function bds_awesome_settings_init(  ) { 
 
-	register_setting( 'plugin_page', 'cd_awesome_settings' );
+	register_setting( 'plugin_page', 'bds_awesome_settings' );
 	
 	add_settings_section(
-		'cd_awesome_plugin_page_section', 
-		__( 'Description for the section', 'codediva' ), 
-		'cd_awesome_settings_section_callback', 
+		'bds_awesome_plugin_page_section', 
+		__( 'Description for the section', 'bradusach' ), 
+		'bds_awesome_settings_section_callback', 
 		'plugin_page'
 	);
 
 	add_settings_field( 
-		'cd_awesome_text_field_0', 
-		__( 'Enter content into the text box', 'codediva' ), 
-		'cd_awesome_text_field_0_render', 
+		'bds_awesome_text_field_0', 
+		__( 'Image 1 URL', 'bradusach' ), 
+		'bds_awesome_text_field_0_render', 
 		'plugin_page', 
-		'cd_awesome_plugin_page_section' 
+		'bds_awesome_plugin_page_section' 
 	);
 	
 	add_settings_field( 
-		'cd_awesome_text_field_1', 
-		__( 'Enter content into the text box', 'codediva' ), 
-		'cd_awesome_text_field_1_render', 
+		'bds_awesome_text_field_1', 
+		__( 'Image 2 URL', 'bradusach' ), 
+		'bds_awesome_text_field_1_render', 
 		'plugin_page', 
-		'cd_awesome_plugin_page_section' 
+		'bds_awesome_plugin_page_section' 
 	);
 
 	add_settings_field( 
-		'cd_awesome_checkbox_field_1', 
-		__( 'Check your preference', 'codediva' ), 
-		'cd_awesome_checkbox_field_1_render', 
+		'bds_awesome_checkbox_field_1', 
+		__( 'Check your preference', 'bradusach' ), 
+		'bds_awesome_checkbox_field_1_render', 
 		'plugin_page', 
-		'cd_awesome_plugin_page_section' 
+		'bds_awesome_plugin_page_section' 
 	);
 
 	add_settings_field( 
-		'cd_awesome_radio_field_2', 
-		__( 'Choose an option', 'codediva' ), 
-		'cd_awesome_radio_field_2_render', 
+		'bds_awesome_radio_field_2', 
+		__( 'Choose an option', 'bradusach' ), 
+		'bds_awesome_radio_field_2_render', 
 		'plugin_page', 
-		'cd_awesome_plugin_page_section' 
+		'bds_awesome_plugin_page_section' 
 	);
+
+
 
 	add_settings_field( 
-		'cd_awesome_textarea_field_3', 
-		__( 'Enter content into the text area', 'codediva' ), 
-		'cd_awesome_textarea_field_3_render', 
+		'bds_awesome_select_field_4', 
+		__( 'Choose from the dropdown', 'bradusach' ), 
+		'bds_awesome_select_field_4_render', 
 		'plugin_page', 
-		'cd_awesome_plugin_page_section' 
-	);
-
-	add_settings_field( 
-		'cd_awesome_select_field_4', 
-		__( 'Choose from the dropdown', 'codediva' ), 
-		'cd_awesome_select_field_4_render', 
-		'plugin_page', 
-		'cd_awesome_plugin_page_section' 
+		'bds_awesome_plugin_page_section' 
 	);
 
 
 }
 
-function cd_awesome_text_field_0_render() { 
-	$options = get_option( 'cd_awesome_settings' );
+function bds_awesome_text_field_0_render() { 
+	$options = get_option( 'bds_awesome_settings' );
 	?>
-	<input type="text" name="cd_awesome_settings[cd_awesome_text_field_0]" value="<?php if (isset($options['cd_awesome_text_field_0'])) echo $options['cd_awesome_text_field_0']; ?>">
+	<input type="text" name="bds_awesome_settings[bds_awesome_text_field_0]" value="<?php if (isset($options['bds_awesome_text_field_0'])) echo $options['bds_awesome_text_field_0']; ?>">
 	<?php
 }
 
 
-function cd_awesome_text_field_1_render() { 
-	$options = get_option( 'cd_awesome_settings' );
+function bds_awesome_text_field_1_render() { 
+	$options = get_option( 'bds_awesome_settings' );
 	?>
-	<input type="text" name="cd_awesome_settings[cd_awesome_text_field_1]" value="<?php if (isset($options['cd_awesome_text_field_1'])) echo $options['cd_awesome_text_field_1']; ?>">
+	<input type="text" name="bds_awesome_settings[bds_awesome_text_field_1]" value="<?php if (isset($options['bds_awesome_text_field_1'])) echo $options['bds_awesome_text_field_1']; ?>">
 	<?php
 }
 
-function cd_awesome_checkbox_field_1_render() { 
-	$options = get_option( 'cd_awesome_settings' );
+function bds_awesome_checkbox_field_1_render() { 
+	$options = get_option( 'bds_awesome_settings' );
 	?>
-	<input type="checkbox" name="cd_awesome_settings[cd_awesome_checkbox_field_1]" <?php if (isset($options['cd_awesome_checkbox_field_1'])) checked( $options['cd_awesome_checkbox_field_1'], 1 ); ?> value="1">
-	<?php
-}
-
-
-function cd_awesome_radio_field_2_render() { 
-	$options = get_option( 'cd_awesome_settings' );
-	?>
-	<input type="radio" name="cd_awesome_settings[cd_awesome_radio_field_2]" <?php if (isset($options['cd_awesome_radio_field_2'])) checked( $options['cd_awesome_radio_field_2'], 1 ); ?> value="1">
+	<input type="checkbox" name="bds_awesome_settings[bds_awesome_checkbox_field_1]" <?php if (isset($options['bds_awesome_checkbox_field_1'])) checked( $options['bds_awesome_checkbox_field_1'], 1 ); ?> value="1">
 	<?php
 }
 
 
-function cd_awesome_textarea_field_3_render() { 
-	$options = get_option( 'cd_awesome_settings' );
+function bds_awesome_radio_field_2_render() { 
+	$options = get_option( 'bds_awesome_settings' );
 	?>
-	<textarea cols="40" rows="5" name="cd_awesome_settings[cd_awesome_textarea_field_3]"> 
-		<?php if (isset($options['cd_awesome_textarea_field_3'])) echo $options['cd_awesome_textarea_field_3']; ?>
- 	</textarea>
+	<input type="radio" name="bds_awesome_settings[bds_awesome_radio_field_2]" <?php if (isset($options['bds_awesome_radio_field_2'])) checked( $options['bds_awesome_radio_field_2'], 1 ); ?> value="1">
 	<?php
 }
 
 
-function cd_awesome_select_field_4_render() { 
-	$options = get_option( 'cd_awesome_settings' );
+
+
+
+function bds_awesome_select_field_4_render() { 
+	$options = get_option( 'bds_awesome_settings' );
 	?>
-	<select name="cd_awesome_settings[cd_awesome_select_field_4]">
-		<option value="1" <?php if (isset($options['cd_awesome_select_field_4'])) selected( $options['cd_awesome_select_field_4'], 1 ); ?>>Option 1</option>
-		<option value="2" <?php if (isset($options['cd_awesome_select_field_4'])) selected( $options['cd_awesome_select_field_4'], 2 ); ?>>Option 2</option>
+	<select name="bds_awesome_settings[bds_awesome_select_field_4]">
+		<option value="1" <?php if (isset($options['bds_awesome_select_field_4'])) selected( $options['bds_awesome_select_field_4'], 1 ); ?>>Option 1</option>
+		<option value="2" <?php if (isset($options['bds_awesome_select_field_4'])) selected( $options['bds_awesome_select_field_4'], 2 ); ?>>Option 2</option>
 	</select>
 <?php
 }
 
 
-function cd_awesome_settings_section_callback() { 
-	echo __( 'More of a description and detail about the section.', 'codediva' );
+function bds_awesome_settings_section_callback() { 
+	echo __( 'More of a description and detail about the section.', 'bradusach' );
 }
 
 
@@ -139,7 +126,7 @@ function my_awesome_plugin_options_page() {
 	?>
 	<form action="options.php" method="post">
 		
-		<h2>My Awesome Plugin</h2>
+		<h2>Image Enlarger Plugin</h2>
 		
 		<?php
 		settings_fields( 'plugin_page' );
@@ -152,13 +139,13 @@ function my_awesome_plugin_options_page() {
 
 }
 
-add_action( 'admin_menu', 'cd_awesome_add_admin_menu' );
-add_action( 'admin_init', 'cd_awesome_settings_init' );	
+add_action( 'admin_menu', 'bds_awesome_add_admin_menu' );
+add_action( 'admin_init', 'bds_awesome_settings_init' );	
 
 
 
 function my_awesome_plugin_callit(){
-	$options = get_option( 'cd_awesome_settings' );
+	$options = get_option( 'bds_awesome_settings' );
 	echo '<style>.image-choose:hover { width: 300px; }</style>';
 	
 	echo '<div style="border: 1px solid black; width: 50%;">';
@@ -177,16 +164,16 @@ function my_awesome_plugin_callit(){
 	echo '<form method="post">';
 	
 	echo '<div style="float: left;">';
-	echo '<img src="' . $options['cd_awesome_text_field_0'] . '" class="image-choose" width="200" height="200" />';
+	echo '<img src="' . $options['bds_awesome_text_field_0'] . '" class="image-choose" width="200" height="200" />';
 	echo '<br />';
-	echo '<input type="radio" name="testname" value="' . $options['cd_awesome_text_field_0'] . '">';
+	echo '<input type="radio" name="testname" value="' . $options['bds_awesome_text_field_0'] . '">';
 	echo '</div>';
 	
 	
 	echo '<div style="float: left;">';
-	echo '<img src="' . $options['cd_awesome_text_field_1'] . '" class="image-choose" width="200" height="200" />';
+	echo '<img src="' . $options['bds_awesome_text_field_1'] . '" class="image-choose" width="200" height="200" />';
 	echo '<br />';
-	echo '<input type="radio" name="testname" value="' . $options['cd_awesome_text_field_1'] . '">';
+	echo '<input type="radio" name="testname" value="' . $options['bds_awesome_text_field_1'] . '">';
 	echo '</div>';
 	
 	
@@ -197,10 +184,10 @@ function my_awesome_plugin_callit(){
 	echo '</form>';
 	
 	
-	echo '<p>Checkbox: ' . $options['cd_awesome_checkbox_field_1'] . '</p>';
-	echo '<p>Radio: ' . $options['cd_awesome_radio_field_2'] . '</p>';
-	echo '<p>Textarea: ' . $options['cd_awesome_textarea_field_3'] . '</p>';
-	echo '<p>Select: ' . $options['cd_awesome_select_field_4'] . '</p>';
+	echo '<p>Checkbox: ' . $options['bds_awesome_checkbox_field_1'] . '</p>';
+	echo '<p>Radio: ' . $options['bds_awesome_radio_field_2'] . '</p>';
+	
+	echo '<p>Select: ' . $options['bds_awesome_select_field_4'] . '</p>';
 }	
 
 add_filter('the_content', 'my_awesome_plugin_callit');	
