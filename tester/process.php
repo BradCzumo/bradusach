6af486ae92a4fe1229854be9b59149454d121790
duki13 <duki_miletic@Dusans-MacBook-Pro-2.local>
2015-03-27 01:11:52 -0400

    display test images

--- tester/process.php
+++ tester/process.php
@@ -1,5 +1,9 @@
 <?php
 
-echo '<div></div>';
+$test = array('http://myworldfacts.com/wp-content/uploads/2015/02/Horse-22.jpg','http://myworldfacts.com/wp-content/uploads/2015/02/Horse-22.jpg','http://myworldfacts.com/wp-content/uploads/2015/02/Horse-22.jpg');
+foreach($test as $item){
+	echo '<img src="' . $item . '" />';
+}
+
 
 ?>
