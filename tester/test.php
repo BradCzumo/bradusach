cb6474b46177b62ee27c81a41eee43c756d1717c
BradCzumo <bradley.czumaczenko@mail.utoronto.ca>
2015-03-27 12:51:51 -0400

    More Pictures available
    Changing what can be chosen for images, added the echo to call them on
    the front-end

--- tester/test.php
+++ tester/test.php
@@ -44,12 +44,29 @@ function bds_awesome_settings_init(  ) {
 	);
 
 	add_settings_field( 
-		'bds_awesome_checkbox_field_1', 
-		__( 'Check your preference', 'bradusach' ), 
-		'bds_awesome_checkbox_field_1_render', 
+		'bds_awesome_text_field_2', 
+		__( 'Image 3 URL', 'bradusach' ), 
+		'bds_awesome_text_field_2_render', 
 		'plugin_page', 
 		'bds_awesome_plugin_page_section' 
-	);
+	);	
+
+	add_settings_field( 
+		'bds_awesome_text_field_3', 
+		__( 'Image 4 URL', 'bradusach' ), 
+		'bds_awesome_text_field_3_render', 
+		'plugin_page', 
+		'bds_awesome_plugin_page_section' 
+	);	
+
+	add_settings_field( 
+		'bds_awesome_text_field_4', 
+		__( 'Image 5 URL', 'bradusach' ), 
+		'bds_awesome_text_field_4_render', 
+		'plugin_page', 
+		'bds_awesome_plugin_page_section' 
+	);		
+
 
 	add_settings_field( 
 		'bds_awesome_radio_field_2', 
@@ -63,7 +80,7 @@ function bds_awesome_settings_init(  ) {
 
 	add_settings_field( 
 		'bds_awesome_select_field_4', 
-		__( 'Choose from the dropdown', 'bradusach' ), 
+		__( 'Select a Color', 'bradusach' ), 
 		'bds_awesome_select_field_4_render', 
 		'plugin_page', 
 		'bds_awesome_plugin_page_section' 
@@ -86,13 +103,25 @@ function bds_awesome_text_field_1_render() {
 	<input type="text" name="bds_awesome_settings[bds_awesome_text_field_1]" value="<?php if (isset($options['bds_awesome_text_field_1'])) echo $options['bds_awesome_text_field_1']; ?>">
 	<?php
 }
-
-function bds_awesome_checkbox_field_1_render() { 
+function bds_awesome_text_field_2_render() { 
 	$options = get_option( 'bds_awesome_settings' );
 	?>
-	<input type="checkbox" name="bds_awesome_settings[bds_awesome_checkbox_field_1]" <?php if (isset($options['bds_awesome_checkbox_field_1'])) checked( $options['bds_awesome_checkbox_field_1'], 1 ); ?> value="1">
+	<input type="text" name="bds_awesome_settings[bds_awesome_text_field_2]" value="<?php if (isset($options['bds_awesome_text_field_2'])) echo $options['bds_awesome_text_field_2']; ?>">
 	<?php
 }
+function bds_awesome_text_field_3_render() { 
+	$options = get_option( 'bds_awesome_settings' );
+	?>
+	<input type="text" name="bds_awesome_settings[bds_awesome_text_field_3]" value="<?php if (isset($options['bds_awesome_text_field_3'])) echo $options['bds_awesome_text_field_3']; ?>">
+	<?php
+}
+function bds_awesome_text_field_4_render() { 
+	$options = get_option( 'bds_awesome_settings' );
+	?>
+	<input type="text" name="bds_awesome_settings[bds_awesome_text_field_4]" value="<?php if (isset($options['bds_awesome_text_field_4'])) echo $options['bds_awesome_text_field_4']; ?>">
+	<?php
+}
+
 
 
 function bds_awesome_radio_field_2_render() { 
@@ -110,15 +139,15 @@ function bds_awesome_select_field_4_render() {
 	$options = get_option( 'bds_awesome_settings' );
 	?>
 	<select name="bds_awesome_settings[bds_awesome_select_field_4]">
-		<option value="1" <?php if (isset($options['bds_awesome_select_field_4'])) selected( $options['bds_awesome_select_field_4'], 1 ); ?>>Option 1</option>
-		<option value="2" <?php if (isset($options['bds_awesome_select_field_4'])) selected( $options['bds_awesome_select_field_4'], 2 ); ?>>Option 2</option>
+		<option value="1" <?php if (isset($options['bds_awesome_select_field_4'])) selected( $options['bds_awesome_select_field_4'], 1 ); ?>>Black</option>
+		<option value="2" <?php if (isset($options['bds_awesome_select_field_4'])) selected( $options['bds_awesome_select_field_4'], 2 ); ?>>Grey</option>
 	</select>
 <?php
 }
 
 
 function bds_awesome_settings_section_callback() { 
-	echo __( 'More of a description and detail about the section.', 'bradusach' );
+	echo __( 'Choose the Prefered image that will be the thumbnail and selected, <br> Edit custom colors as well </br>', 'bradusach' );
 }
 
 
@@ -146,9 +175,11 @@ add_action( 'admin_init', 'bds_awesome_settings_init' );
 
 function my_awesome_plugin_callit(){
 	$options = get_option( 'bds_awesome_settings' );
+	// Enables hover over the images we have linked with a designated size in the Css
 	echo '<style>.image-choose:hover { width: 300px; }</style>';
-	
+	// Black border around the text box around the image, echoing out the Http to make it
 	echo '<div style="border: 1px solid black; width: 50%;">';
+	// If then for when the radio button from the back end is checked off, the image and selected will enlarge  to the size of 700
 	if (isset($_POST['testname'])) {
 		$selected_radio = $_POST['testname'];
 		echo '<img src="' . $selected_radio . '" width="700" />';
@@ -163,6 +194,9 @@ function my_awesome_plugin_callit(){
 	
 	echo '<form method="post">';
 	
+	/*beginning of echoing out all the text fields that are defined in the back end. This will call them on the 
+	front end to the image the user has chosen
+	*/
 	echo '<div style="float: left;">';
 	echo '<img src="' . $options['bds_awesome_text_field_0'] . '" class="image-choose" width="200" height="200" />';
 	echo '<br />';
@@ -176,18 +210,36 @@ function my_awesome_plugin_callit(){
 	echo '<input type="radio" name="testname" value="' . $options['bds_awesome_text_field_1'] . '">';
 	echo '</div>';
 	
+	echo '<div style="float: left;">';
+	echo '<img src="' . $options['bds_awesome_text_field_2'] . '" class="image-choose" width="200" height="200" />';
+	echo '<br />';
+	echo '<input type="radio" name="testname" value="' . $options['bds_awesome_text_field_2'] . '">';
+	echo '</div>';
+	
+	echo '<div style="float: left;">';
+	echo '<img src="' . $options['bds_awesome_text_field_3'] . '" class="image-choose" width="200" height="200" />';
+	echo '<br />';
+	echo '<input type="radio" name="testname" value="' . $options['bds_awesome_text_field_3'] . '">';
+	echo '</div>';
+
+	echo '<div style="float: left;">';
+	echo '<img src="' . $options['bds_awesome_text_field_4'] . '" class="image-choose" width="200" height="200" />';
+	echo '<br />';
+	echo '<input type="radio" name="testname" value="' . $options['bds_awesome_text_field_4'] . '">';
+	echo '</div>';
+	
 	
 	echo '<br />';
 	
 	
 	echo '<input type="submit" name="dlsubmit" value="Enlarge">';
+	
 	echo '</form>';
 	
 	
-	echo '<p>Checkbox: ' . $options['bds_awesome_checkbox_field_1'] . '</p>';
 	echo '<p>Radio: ' . $options['bds_awesome_radio_field_2'] . '</p>';
 	
-	echo '<p>Select: ' . $options['bds_awesome_select_field_4'] . '</p>';
+
 }	
 
 add_filter('the_content', 'my_awesome_plugin_callit');	
